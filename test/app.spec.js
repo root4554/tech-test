d5a7e27d532b2f3a3a102e9c8252dcf8ac92bc86
root4554 <anass.kbir@hotmail.com>
2023-01-17 21:10:18 +0100

    first commit

---
+++ test/app.spec.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+
+describe('app test', () => {
+
+  describe('main function', () => {
+
+    it('should just pass!', () => {
+      expect('pass').to.equal('pass');
+    });
+
+  });
+
+});
